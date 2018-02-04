20230778e03352d8a51a915b2bc252733e0f991c
iirlas <iirlas@yahoo.com>
2018-02-04 03:59:45 -0600

    Added Assets\Scripts\Discriptor.cs

---
+++ Assets/Scripts/Discriptor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Marine/Discriptor")]
+public class Discriptor : ScriptableObject
+{
+    public int minLifeSpan;
+    public int maxLifeSpan;
+    public AnimatorOverrideController controller;
+    public Material[] materials;
+
+    public Attributes Create ()
+    {
+        Attributes attributes = ScriptableObject.CreateInstance<Attributes>();
+        attributes.controller = controller;
+        attributes.lifespan = Random.Range(minLifeSpan, maxLifeSpan);
+        attributes.material = materials[Random.Range(0, materials.Length)];
+        attributes.age = 0;
+
+        return attributes;
+    }
+}
