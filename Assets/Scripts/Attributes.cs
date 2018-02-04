005e62a24a1f002fff856817fc728afe26084e86
iirlas <iirlas@yahoo.com>
2018-02-03 22:05:13 -0600

    Added Assets\Scripts\Attributes.cs Added Assets\Scripts\Configure.cs

---
+++ Assets/Scripts/Attributes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[CreateAssetMenu(menuName="Marine/Attributes")]
+public class Attributes : ScriptableObject
+{
+    private System.DateTime creation;
+    public AnimatorOverrideController controller;
+    public Material material;
+    public int health = 10;
+    public int age
+    {
+        set
+        {
+            DateTime now = System.DateTime.Now;
+            creation = new DateTime(
+                now.Year,
+                now.Month,
+                now.Day,
+                now.Hour,
+                now.Minute + value,
+                now.Second
+            );
+        }
+        get
+        {
+            return (System.DateTime.Now - creation).Minutes;
+        }
+    }
+}
---
+++ Assets/Scripts/Configure.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Renderer))]
+public class Life : MonoBehaviour
+{
+    [HideInInspector]
+    public Animator animator;
+    [HideInInspector]
+    public new Renderer renderer;
+    public Attributes attributes;
+
+    private void OnValidate ()
+    {
+        if (attributes)
+        {
+            animator = GetComponent<Animator>();
+            if (animator)
+            {
+                animator.runtimeAnimatorController = attributes.controller;
+            }
+            renderer = GetComponent<Renderer>();
+            if (renderer)
+            {
+                renderer.material = attributes.material;
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        animator.runtimeAnimatorController = attributes.controller;
+    }
+}
