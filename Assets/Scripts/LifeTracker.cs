2eb0e17e55c0c1885d75252e56d1e9ad712634de
iirlas <iirlas@yahoo.com>
2018-02-04 22:22:54 -0600

    Added Assets\Scripts\LifeTracker.cs Added Assets\Scripts\Valuator.cs

---
+++ Assets/Scripts/LifeTracker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeTracker : ScriptableObject
+{
+    private static LifeTracker lifeTracker;
+    public static LifeTracker instance
+    {
+        get
+        {
+            if (!lifeTracker)
+            {
+                lifeTracker = ScriptableObject.CreateInstance<LifeTracker>();
+            }
+            return lifeTracker;
+        }
+    }
+
+    public List<Attributes> attributes = new List<Attributes>();
+}
