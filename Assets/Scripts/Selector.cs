01627a8363f7b28ce7810a46e6b1e3c0c62fb7a9
iirlas <iirlas@yahoo.com>
2018-02-05 20:51:15 -0600

    Added Assets\Scripts\Selector.cs
    Revised Assets\Prefabs\Inhabitant.prefab

---
+++ Assets/Scripts/Selector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Inhabitant))]
+[RequireComponent(typeof(Collider2D))]
+public class Selector : MonoBehaviour
+{
+    private void OnMouseDown ()
+    {
+        Tracker.instance.selected = GetComponent<Inhabitant>();
+    }
+}
